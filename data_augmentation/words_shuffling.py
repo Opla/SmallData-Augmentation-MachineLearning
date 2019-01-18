68595d5884fc38e487f6b8303dc71b834635a1c6
maali-mnasri <maali-mnasri@users.noreply.github.com>
2019-01-18 17:33:42 +0100

    Update words_shuffling.py

--- data_augmentation/words_shuffling.py
+++ data_augmentation/words_shuffling.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2015-present, CWB SAS
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
+__author__ = "Maali Mnasri"
+__copyright__ = "Copyright (c) 2015-present, CWB SAS - All Rights Reserved"
+"""
+
 from nltk import word_tokenize
 import random
 
